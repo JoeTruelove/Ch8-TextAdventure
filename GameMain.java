cb99da52bffabb6240c3b6a8bc488b270cfcb076
JoeTruelove <43154676+JoeTruelove@users.noreply.github.com>
2018-11-05 22:38:42 -0500

    added main
    Created GameMain class to execute the game through cmd

---
+++ GameMain.java
@@ -0,0 +1,19 @@
+
+/**
+ * GameMain class that creates a game object and starts 
+ *
+ * @author (Joseph Truelove)
+ * @version (2018.11.05)
+ */
+public class GameMain
+{
+    public static void main(String[] args)
+    {
+        
+
+        //create a new game object
+        Game game = new Game();
+        //call the play method to start game
+        game.play();
+    }
+}
